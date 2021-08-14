0dd8efd996d15eeef187c88953fb924d6e96c5db
sun_loves_Q <conconxel@gmail.com>
2021-08-15 00:42:26 +0900

    i morgan

--- src/server.js
+++ src/server.js
@@ -1,24 +1,25 @@
 
 import express from "express";
+import morgan from "morgan";
 
-const PORT = 4000
+const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const handleHome = (req, res) => {
-    return res.send("<h1>I love you</h1>");
+const home = (req, res) => {
+  return res.send("hello");
+};
+const login = (req, res) => {
+  return res.send("login");
 };
 
-const handleLogin = (req, res) => {
-    return res.send({message:"Login here."});
-};
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening)
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
 
 
