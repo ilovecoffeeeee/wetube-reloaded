eca58f89268e0f32c9c38135a5bf390de059751f
sun_loves_Q <conconxel@gmail.com>
2021-08-17 06:36:30 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,19 +4,30 @@ import morgan from "morgan";
 
 const PORT = 4000;
 
-
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
-  next();
+const globalRouter = express.Router();
+const userRouter = express.Router();
+const videoRouter = express.Router();
+
+
+
+
+const home = (req, res) => {
+  return res.send("hello");
 };
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
+
+const login = (req, res) => {
+  return res.send("login");
 };
-app.get("/", gossipMiddleware, handleHome);
+
+
 
 const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
+
+app.listen(PORT, handleListening);
 
 
