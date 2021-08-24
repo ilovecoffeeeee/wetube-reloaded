861e83b748523bf28384adb3114bcb61d872c751
sun_loves_Q <conconxel@gmail.com>
2021-08-24 23:25:30 +0900

    get and post

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import { 
+    watch, 
+    getUpload,
+    getEdit,
+    postEdit,
+    postUpload,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.route("/:id(\\d+)").get(watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
