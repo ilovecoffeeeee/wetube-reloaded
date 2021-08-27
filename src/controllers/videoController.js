2b75e2126fe5e1e7ef6c192b72e3e5302ddd408c
sun_loves_Q <conconxel@gmail.com>
2021-08-28 02:03:27 +0900

    oh my head

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -30,10 +30,14 @@ export const getEdit = async (req, res) => {
 
 export const postEdit = async (req, res) => {
     const { id } = req.params;
-    const video = await Video.findById(id);
+    const { title, description, hashtags } = req.body;
+    const video = await Video.exists({_id:id});
     if(!video) {
         return res.render("404", { pageTitle: "Video not found."});
     }
+    await Video.findByIdAndUpdate(id, {
+        title, description, hashtags:hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    })
     return res.redirect(`/videos/${id}`);
 };
 
@@ -47,7 +51,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title,
             description,
-            hashtags: hashtags.split(",").map(((word) => word.startWith("#") ? word : `#${word}`)),
+            hashtags: hashtags.split(",").map(((word) => word.startsWith("#") ? word : `#${word}`)),
         });
         return res.redirect("/");
     } catch(error) {
