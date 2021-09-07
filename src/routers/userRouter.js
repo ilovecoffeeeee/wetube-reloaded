cb67e091a6a43ccad9109575acc2034cf66a5d98
sun_loves_Q <conconxel@gmail.com>
2021-09-08 02:29:31 +0900

    Edit Profile

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { edit, logout, startGithubLogin, finishGithubLogin, see } from "../controllers/userController"
+import { getEdit, postEdit, logout, startGithubLogin, finishGithubLogin, see } from "../controllers/userController"
+import { protectordMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/logout", protectordMiddleware, logout);
+userRouter.route("/edit").all(protectordMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,12 +7,13 @@ import {
     postUpload,
     deleteVideo,
 } from "../controllers/videoController";
+import { protectordMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.route("/:id([0-9a-f]{24})").get(watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter.route("/:id([0-9a-f]{24})/edit").all(protectordMiddleware).get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").all(protectordMiddleware).get(deleteVideo);
+videoRouter.route("/upload").all(protectordMiddleware).get(getUpload).post(postUpload);
 
 export default videoRouter;
