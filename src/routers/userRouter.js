a2f1fe9bdedf21cea801364ed2e32adbe038e4b1
sun_loves_Q <conconxel@gmail.com>
2021-09-09 22:58:13 +0900

    upload avavtar

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,13 +1,15 @@
 import express from "express";
-import { getEdit, postEdit, logout, startGithubLogin, finishGithubLogin, see } from "../controllers/userController"
-import { protectordMiddleware, publicOnlyMiddleware } from "../middlewares";
+import { getEdit, postEdit, logout, startGithubLogin, finishGithubLogin, see, getChangePassword, postChangePassword } from "../controllers/userController"
+import { protectordMiddleware, publicOnlyMiddleware, uploadFiles } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectordMiddleware, logout);
-userRouter.route("/edit").all(protectordMiddleware).get(getEdit).post(postEdit);
+userRouter.route("/edit").all(protectordMiddleware).get(getEdit).post(uploadFiles.single("avatar"), postEdit);
+userRouter.route("/change-password").all(protectordMiddleware).get(getChangePassword).post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
+
 userRouter.get(":id", see);
 
 export default userRouter;
