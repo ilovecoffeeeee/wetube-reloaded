e6a502019507365379c22618366b52457d50a7d9
sun_loves_Q <conconxel@gmail.com>
2021-09-24 01:16:58 +0900

    controller events

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,9 +1,15 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currenTime = document.getElementById("currenTime");
+const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreen");
+const videoContainer = document.getElementById("videoContainer");
+const videoControls = document.getElementById("videoControls");
 
+let controllsTimeout = null;
 let volumeValue = 0.5
 video.volume = volumeValue;
 
@@ -37,6 +43,56 @@ const handleVolumeChange = (event) => {
     video.volume = value;
 }
 
+const formatTime = (seconds) => new Date(seconds*1000).toISOString().substr(11, 8);
+
+const handleLoadedMetadata = () => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max =  Math.floor(video.duration);
+};
+
+const handleTimeUpdate = ()=> {
+    currenTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimelineChange = (event) => {
+    const {
+        target:{value},
+    } = event;
+    video.currentTime = value;
+};
+
+const handleFullScreen = () => {
+    const fullscreen = document.fullscreenElement;
+    if(fullscreen) {
+        document.exitFullscreen();
+        fullScreenBtn.innerText = "Enter Full Screen"
+    } else {
+        videoContainer.requestFullscreen();
+        fullScreenBtn.innerText = "Exit Full Screen"
+    };
+};
+
+const handleMouseMove = () => {
+    if(controllsTimeout){
+        clearTimeout(controllsTimeout);
+        controllsTimeout = null;
+    }
+    videoControls.classList.add("showing");
+}
+
+const handelMouseLeave = () =>{
+    controllsTimeout = setTimeout(() => {
+        videoControls.classList.remove("showing");
+    }, 3000)
+}
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimelineChange);
+fullScreenBtn.addEventListener("click", handleFullScreen);
+video.addEventListener("mousemove", handleMouseMove);
+video.addEventListener("mouseleave", handelMouseLeave);
