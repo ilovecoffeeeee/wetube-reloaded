36ee7ac9544e16ff42f0c11d1723cdc3a543000a
sun_loves_Q <conconxel@gmail.com>
2021-10-13 02:59:45 +0900

    comment 2

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,17 +1,39 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
+const addComment = (text) => {
+    const videoComments = document.querySelector(".video__comments ul");
+    const newComment = document.createElement("li");
+    newComment.className = "video__comment";
+    const icon = document.createElement("i");
+    icon.className = "fas fa-comment"
+    const span = document.createElement("span");
+    span.innerText = ` ${text}`;
+    newComment.appendChild(icon);
+    newComment.appendChild(span);
+    videoComments.prepend(newComment);
+}
 
-const handleSubmit = (event) => {
+
+const handleSubmit = async(event) => {
     event.preventDefault();
     const textarea = form.querySelector("textarea");
     const text = textarea.value;
     const videoId = videoContainer.dataset.id;
-    fetch(`/api/videos/${videoId}/comment`, {
+    if (text === "")  {
+        return;
+    }
+    const { status } = await fetch(`/api/videos/${videoId}/comment`, {
         method: "POST",
-        body: {
-            text,
+        headers:  {
+            "Content-Type": "application/json" 
         },
+        body: JSON.stringify({ text }),
     });
+    if (status === 201) {
+        addComment(text);
+    }
+    textarea.value = "";
+    
 };
 
 if (form) {
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,7 @@
-import Video from"../models/Video"
-import User from"../models/User"
+import Video from"../models/Video";
+import Comment from"../models/Comment";
+import User from"../models/User";
+import { Collection } from "mongoose";
 
 export const home = async (req, res) => {
     try {     
@@ -12,7 +14,8 @@ export const home = async (req, res) => {
 
 export const watch = async (req,res) => {
     const { id } = req.params;
-    const video = await Video.findById(id).populate("owner");
+    const video = await Video.findById(id).populate("owner").populate("comments");
+    console.log(video);
     if(!video){
         return res.render("404", { pageTitle: "Video not found.",});
     }
@@ -128,8 +131,23 @@ export const registerView = async (req, res) => {
     return res.sendStatus(200);
 };
 
-export const createComment = (req, res) => {
-    console.log(req.params);
-    console.log(req.body);
-    return res.end();
+export const createComment = async(req, res) => {
+    const {
+        session: { user },
+        body: { text },
+        params: { id },
+    } = req;
+    
+    const video = await Video.findById(id);
+    if (!video){
+        return res.sendStatus(404);
+    };
+    const comment = await Comment.create({
+        text,
+        owner: user._id,
+        video: id,
+    });
+    video.comments.push(comment._id);
+    video.save();
+    return res.sendStatus(201);
 }
